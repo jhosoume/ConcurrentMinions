0e3b45e41e53d53c8e4ad977fbf3fa9cf1de8a6e
Juliana Hosoume <ju.hosoume@gmail.com>
2019-06-02 22:55:43 -0300

    Fix num interactions

--- src/game_theory.cpp
+++ src/game_theory.cpp
@@ -5,20 +5,30 @@ bool calculating = false;
 int interactions = 0;
 pthread_mutex_t lock_minions = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lock_interactions = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t gru_cond = PTHREAD_COND_INITIALIZER;
+bool gru_recruiting = false;
 
 
 void *fminion(void *identifier) {
   int id = *((int *) identifier);
   free((int *)identifier);
-  int partner;
+  int partner = id;
   int sleep_time;
 
   // cout << "Created Minion id = " << id << " | Strategy = "
     // << minions[id].strategy.name << endl;
   sleep(limited_rand(0, 3));
   while (1) {
-    partner = limited_rand(0, NUM_MINIONS - 1);
+    while (partner == id) {
+      partner = limited_rand(0, NUM_MINIONS - 1);
+    }
     pthread_mutex_lock(&lock_minions);
+    cout << "Num interactions 1 = " << interactions << endl;
+    if (interactions >= NUM_INTERACTIONS) {
+      pthread_mutex_unlock(&lock_minions);
+      break;
+    }
+    ++interactions;
     while (minions[id].occupied || minions[partner].occupied) {
       if (minions[id].occupied) {
         cout << "Minion " << id << " is already in an interaction." << endl;
@@ -32,6 +42,7 @@ void *fminion(void *identifier) {
     minions[partner].occupied = true;
     sleep_time = minions[id].strategy.interaction_duration;
     cout << "Minion " << id << " is interacting with " << partner << endl;
+    cout << "Num interactions 2 = " << interactions << endl;
     pthread_mutex_unlock(&lock_minions);
 
     sleep(sleep_time);
@@ -42,15 +53,9 @@ void *fminion(void *identifier) {
     pthread_cond_broadcast(&minions[id].mcond);
     pthread_cond_broadcast(&minions[partner].mcond);
     cout << "Minion " << id << " is done with " << partner << endl;
-    cout << "Num interactions = " << interactions << endl;
-    if (interactions >= NUM_INTERACTIONS) {
-      pthread_mutex_unlock(&lock_minions);
-      break;
-    }
-    ++interactions;
     pthread_mutex_unlock(&lock_minions);
     // cout << "Interactions = " << interactions << endl;
-    sleep(5);
+    // sleep(5);
 
   }
   cout << "DONE!" << endl;
