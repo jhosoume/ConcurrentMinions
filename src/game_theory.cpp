ed0070cbd2334b62b934ba34a7d5640f0716947d
Juliana Hosoume <ju.hosoume@gmail.com>
2019-06-03 16:15:48 -0300

    Fix Gru

--- src/game_theory.cpp
+++ src/game_theory.cpp
@@ -8,7 +8,7 @@ pthread_mutex_t lock_interactions = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t gru_cond = PTHREAD_COND_INITIALIZER;
 bool gru_recruiting = false;
 int to_recruit = 0;
-sem_t gru_generation;
+bool gru_generation = false;
 sem_t gru_recruited;
 list <int> minions_recruited;
 
@@ -20,11 +20,13 @@ void *fgru(void *identifier) {
 
   while(1) {
     sleep(GRU_WAIT);
-    sem_wait(&gru_generation);
+    to_recruit = limited_rand(min_minions, max_minions);
+    to_recruit = 10;
 
     pthread_mutex_lock(&lock_minions);
-    to_recruit = limited_rand(min_minions, max_minions);
-    to_recruit = 8;
+    while (!gru_generation) {
+      pthread_cond_wait(&gru_cond, &lock_minions);
+    }
     cout << "!!! Gru is recruiting " << to_recruit << " minions!" << endl;
     gru_recruiting = true;
     pthread_mutex_unlock(&lock_minions);
@@ -67,12 +69,6 @@ void *fminion(void *identifier) {
       partner = limited_rand(0, NUM_MINIONS - 1);
     }
     pthread_mutex_lock(&lock_minions);
-    if (interactions >= NUM_INTERACTIONS) {
-      sem_trywait(&gru_generation);
-      pthread_mutex_unlock(&lock_minions);
-      break;
-    }
-    ++interactions;
     while (minions[id].occupied ||
            minions[partner].occupied ||
            (gru_recruiting && (to_recruit > 0))) {
@@ -94,6 +90,12 @@ void *fminion(void *identifier) {
         pthread_cond_wait(&minions[partner].mcond, &lock_minions);
       }
     }
+    if (interactions >= NUM_INTERACTIONS) {
+      gru_generation = false;
+      pthread_mutex_unlock(&lock_minions);
+      break;
+    }
+    ++interactions;
     minions[id].occupied = true;
     minions[partner].occupied = true;
     sleep_time = minions[id].strategy.interaction_duration;
@@ -125,13 +127,15 @@ int main() {
   pthread_t minions_threads[NUM_MINIONS];
   pthread_t gru;
   int *id;
-  sem_init(&gru_generation, 0, 0);
   sem_init(&gru_recruited, 0, 0);
 
   Strategies strats = Strategies();
   Generations generation = Generations(NUM_INTERACTIONS);
   generation.initializeMinions(strats);
-  sem_post(&gru_generation);
+  pthread_mutex_lock(&lock_minions);
+  gru_generation = true;
+  pthread_cond_signal(&gru_cond);
+  pthread_mutex_unlock(&lock_minions);
 
   for (int min_indx = 0; min_indx < NUM_MINIONS; ++min_indx) {
     id = (int *) malloc(sizeof(int));
